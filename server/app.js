aaf2965823ca587bd68dd5d39772d9c9c4b244b1
MuRiKeilendt <keilendtmauricio@gmail.com>
2024-01-17 00:05:18 -0300

    intento 3

--- server/app.js
+++ server/app.js
@@ -6,7 +6,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 // Habilita CORS para todas las rutas
-app.use(cors())
+const corsOptions = {
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}
+app.use(cors(corsOptions))
 
 
 // Middleware para parsear el cuerpo de la solicitud como JSON
