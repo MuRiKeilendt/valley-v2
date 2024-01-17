82abcbd32273c506dfdca2cb60dd8969379ca7f1
MuRiKeilendt <keilendtmauricio@gmail.com>
2024-01-16 23:57:32 -0300

    try 900

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -342,7 +342,7 @@ export default {
         this.loading = true
         const response = await fetch(apiUrl, {
           method: 'GET',
-          mode: 'cors',
+          mode: 'no-cors',
         })
         const data = await response.json()
 
